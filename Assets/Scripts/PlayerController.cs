5dcbb767d5e3569f9ef9ce57a801fd85e36dd974
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-01-24 07:23:02 -0200

    Fixing mixed tabs and spaces

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,146 +3,146 @@ using System.Collections;
 
 enum PlayerDirection
 {
-	None, Up, Down, Left, Right
+    None, Up, Down, Left, Right
 }
 
 enum PlayerItem
 {
-	None = 0,
-	Sword,
-	Boots,
-	PowerGloves
+    None = 0,
+    Sword,
+    Boots,
+    PowerGloves
 }
 
 [RequireComponent(typeof(TiledCharacter))]
 public class PlayerController : MonoBehaviour
 {
-	private TiledCharacter tiled;
+    private TiledCharacter tiled;
 
-	[SerializeField]
-	private float moveDuration;
-	[SerializeField]
-	private float moveInterval;
+    [SerializeField]
+    private float moveDuration;
+    [SerializeField]
+    private float moveInterval;
 
-	[SerializeField, HideInInspector]
-	private PlayerDirection direction;
-	private PlayerDirection new_direction;
-	[SerializeField, HideInInspector]
-	private PlayerItem equippedItem;
+    [SerializeField, HideInInspector]
+    private PlayerDirection direction;
+    private PlayerDirection new_direction;
+    [SerializeField, HideInInspector]
+    private PlayerItem equippedItem;
 
-	private bool controllable;
+    private bool controllable;
 
-	void Awake()
-	{
-		tiled = GetComponent<TiledCharacter>();
+    void Awake()
+    {
+        tiled = GetComponent<TiledCharacter>();
         direction = PlayerDirection.None;
         new_direction = PlayerDirection.None;
         equippedItem = PlayerItem.None;
-	}
+    }
 
-	void Start()
-	{
-		bool first_player = FindObjectsOfType(typeof(PlayerController)).Length <= 1;
-		Debug.Log(FindObjectsOfType(typeof(PlayerController)).Length);
-		controllable = first_player;
+    void Start()
+    {
+        bool first_player = FindObjectsOfType(typeof(PlayerController)).Length <= 1;
+        Debug.Log(FindObjectsOfType(typeof(PlayerController)).Length);
+        controllable = first_player;
 
-		if (!controllable)
-		{
-			SpriteRenderer renderer = GetComponent<SpriteRenderer>();
-			Color col = renderer.material.color;
-			col.a = 0.5f;
-			renderer.material.color = col;
-		}
+        if (!controllable)
+        {
+            SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+            Color col = renderer.material.color;
+            col.a = 0.5f;
+            renderer.material.color = col;
+        }
 
-		StartCoroutine(movementLoop());
-	}
+        StartCoroutine(movementLoop());
+    }
 
-	void Update()
-	{
-		if (controllable)
-		{
-			if (Input.GetKeyDown(KeyCode.UpArrow)) { new_direction = PlayerDirection.Up; }
-			if (Input.GetKeyDown(KeyCode.DownArrow)) { new_direction = PlayerDirection.Down; }
-			if (Input.GetKeyDown(KeyCode.LeftArrow)) { new_direction = PlayerDirection.Left; }
-			if (Input.GetKeyDown(KeyCode.RightArrow)) { new_direction = PlayerDirection.Right; }
+    void Update()
+    {
+        if (controllable)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow)) { new_direction = PlayerDirection.Up; }
+            if (Input.GetKeyDown(KeyCode.DownArrow)) { new_direction = PlayerDirection.Down; }
+            if (Input.GetKeyDown(KeyCode.LeftArrow)) { new_direction = PlayerDirection.Left; }
+            if (Input.GetKeyDown(KeyCode.RightArrow)) { new_direction = PlayerDirection.Right; }
 
-			if (Input.GetKeyDown(KeyCode.Alpha1)) { equippedItem = PlayerItem.Sword; }
-			if (Input.GetKeyDown(KeyCode.Alpha2)) { equippedItem = PlayerItem.Boots; }
-			if (Input.GetKeyDown(KeyCode.Alpha3)) { equippedItem = PlayerItem.PowerGloves; }
-		}
-	}
+            if (Input.GetKeyDown(KeyCode.Alpha1)) { equippedItem = PlayerItem.Sword; }
+            if (Input.GetKeyDown(KeyCode.Alpha2)) { equippedItem = PlayerItem.Boots; }
+            if (Input.GetKeyDown(KeyCode.Alpha3)) { equippedItem = PlayerItem.PowerGloves; }
+        }
+    }
 
-	private IEnumerator movementLoop() {
-		while (true)
-		{
-			if (direction != new_direction)
-			{
-				if (direction != PlayerDirection.None)
-				{
-					// Split player
-					GameObject new_player_go = (GameObject)Instantiate(gameObject);
+    private IEnumerator movementLoop() {
+        while (true)
+        {
+            if (direction != new_direction)
+            {
+                if (direction != PlayerDirection.None)
+                {
+                    // Split player
+                    GameObject new_player_go = (GameObject)Instantiate(gameObject);
                     var player_controller = new_player_go.GetComponent<PlayerController>();
                     player_controller.new_direction = direction;
                     player_controller.direction = PlayerDirection.None;
-				}
-			}
-			direction = new_direction;
-			if (direction != PlayerDirection.None)
-			{
-				int x = tiled.TileX;
-				int y = tiled.TileY;
-				Vector2 dir = Vector2.zero;
-				switch (direction)
-				{
-					case PlayerDirection.Up:
-						y += 1;
-						dir = Vector2.up;
-						break;
-					case PlayerDirection.Down:
-						y -= 1;
-						dir = -Vector2.up;
-						break;
-					case PlayerDirection.Left:
-						x -= 1;
-						dir = -Vector2.right;
-						break;
-					case PlayerDirection.Right:
-						x += 1;
-						dir = Vector2.right;
-						break;
-					default: break;
-				}
+                }
+            }
+            direction = new_direction;
+            if (direction != PlayerDirection.None)
+            {
+                int x = tiled.TileX;
+                int y = tiled.TileY;
+                Vector2 dir = Vector2.zero;
+                switch (direction)
+                {
+                    case PlayerDirection.Up:
+                        y += 1;
+                        dir = Vector2.up;
+                        break;
+                    case PlayerDirection.Down:
+                        y -= 1;
+                        dir = -Vector2.up;
+                        break;
+                    case PlayerDirection.Left:
+                        x -= 1;
+                        dir = -Vector2.right;
+                        break;
+                    case PlayerDirection.Right:
+                        x += 1;
+                        dir = Vector2.right;
+                        break;
+                    default: break;
+                }
 
-				if (tiled.CollideWithBlocker(dir))
-				{
-					direction = PlayerDirection.None;
-				}
-				else
-				{
-					tiled.MoveToTile(x, y, moveDuration);
-				}
-			}
+                if (tiled.CollideWithBlocker(dir))
+                {
+                    direction = PlayerDirection.None;
+                }
+                else
+                {
+                    tiled.MoveToTile(x, y, moveDuration);
+                }
+            }
 
-			yield return new WaitForSeconds(moveInterval);
-		}
-	}
+            yield return new WaitForSeconds(moveInterval);
+        }
+    }
 
-	void OnTriggerEnter2D(Collider2D other)
-	{
-		if (other.CompareTag("slime"))
-		{
-			if (equippedItem == PlayerItem.Sword)
-			{
-				Destroy(other.gameObject);
-			}
-			else
-			{
-				Destroy(gameObject);
-			}
-		}
-		else if (other.CompareTag("exit_door"))
-		{
-			Destroy(gameObject);
-		}
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("slime"))
+        {
+            if (equippedItem == PlayerItem.Sword)
+            {
+                Destroy(other.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (other.CompareTag("exit_door"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
