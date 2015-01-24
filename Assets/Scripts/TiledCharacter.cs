cc13a0d1e504bf19642ae7bac43ff2d2855b5ff4
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-01-24 01:17:39 -0200

    Some VERY basic movimentation.

---
+++ Assets/Scripts/TiledCharacter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tiled2Unity;
+
+[ExecuteInEditMode]
+public class TiledCharacter : MonoBehaviour {
+    [SerializeField]
+    private TiledMap mapData;
+    [SerializeField]
+    private int TileX;
+    [SerializeField]
+    private int TileY;
+    [SerializeField]
+    private Vector3 BaseOffset;
+
+    void Update() {
+        transform.position = mapData.transform.position + BaseOffset + 
+                             new Vector3(TileX * 2f * BaseOffset.x, TileY * 2f * BaseOffset.y, 0f);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            --TileY;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            ++TileY;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            --TileX;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            ++TileX;
+        }
+    }
+}
