5993d725f85b323c08b083a61aab588dbe8bf17c
Yuri Kunde Schlesner <yuriks@yuriks.net>
2015-01-25 15:01:19 -0200

    Update levels

---
+++ External/Tmx/Gameplay.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Gameplay" tilewidth="41" tileheight="60">
+ <tile id="0">
+  <properties>
+   <property name="AddComp" value="Touchplate"/>
+  </properties>
+  <image width="41" height="41" source="../../Assets/Sprites/Cave/Botão_001.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="AddComp" value="Crate"/>
+  </properties>
+  <image width="40" height="60" source="../../Assets/Sprites/Cave/Box_001.PNG"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="AddComp" value="Arrow"/>
+  </properties>
+  <image width="40" height="40" source="../../Assets/Sprites/arrow.png"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="AddComp" value="Hole"/>
+  </properties>
+  <image width="40" height="40" source="../../Assets/Sprites/hole.png"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="AddComp" value="InvisibleExitSmall"/>
+  </properties>
+  <image width="40" height="40" source="../../Assets/Sprites/exit_door.png"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="AddComp" value="Slime"/>
+  </properties>
+  <image width="40" height="40" source="../../Assets/Sprites/slime.png"/>
+ </tile>
+</tileset>
---
+++ External/Tmx/NewDungeon.tsx
@@ -0,0 +1,93 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="NewDungeon" tilewidth="45" tileheight="82">
+ <tile id="0">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Ground Tile/Ground_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Ground Tile/Ground_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Ground Tile/Ground_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Ground Tile/Ground_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole.png"/>
+ </tile>
+ <tile id="5">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1x1.png"/>
+ </tile>
+ <tile id="6">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xa.png"/>
+ </tile>
+ <tile id="7">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xaa.png"/>
+ </tile>
+ <tile id="8">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xb.png"/>
+ </tile>
+ <tile id="9">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xbb.png"/>
+ </tile>
+ <tile id="10">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xc.png"/>
+ </tile>
+ <tile id="11">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xcc.png"/>
+ </tile>
+ <tile id="12">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xL.png"/>
+ </tile>
+ <tile id="13">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xLL.png"/>
+ </tile>
+ <tile id="14">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xT.png"/>
+ </tile>
+ <tile id="15">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole1xX.png"/>
+ </tile>
+ <tile id="16">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole2x2a.png"/>
+ </tile>
+ <tile id="17">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole2x2b.png"/>
+ </tile>
+ <tile id="18">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole2x2c.png"/>
+ </tile>
+ <tile id="19">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Holes/Hole2x2d.png"/>
+ </tile>
+ <tile id="20">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Paredes/Parede.png"/>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="40" height="40"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Paredes/Paredes1xT.png"/>
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="40" height="40"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <image width="45" height="82" source="../../Assets/Sprites/Cave/Porta_Centro_001.png"/>
+ </tile>
+ <tile id="23">
+  <image width="38" height="39" source="../../Assets/Sprites/Cave/Porta_Entrada_001.png"/>
+ </tile>
+ <tile id="24">
+  <image width="40" height="40" source="../../Assets/Sprites/Cave/Paredes/ParedesSuperior.png"/>
+ </tile>
+ <tile id="25">
+  <image width="36" height="37" source="../../Assets/Sprites/Cave/Bestas/Besta_bot_001.png"/>
+ </tile>
+ <tile id="26">
+  <image width="36" height="37" source="../../Assets/Sprites/Cave/Bestas/Besta_Sides_001.png"/>
+ </tile>
+ <tile id="27">
+  <image width="36" height="37" source="../../Assets/Sprites/Cave/Bestas/Besta_Top_001.png"/>
+ </tile>
+</tileset>
