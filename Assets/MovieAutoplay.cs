67eafb4a4bdb33946b07d7eeeda04b4d55bf1fe8
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-01-25 15:50:57 -0200

    Adding animations

---
+++ Assets/MovieAutoplay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovieAutoplay : MonoBehaviour {
+    public MovieTexture movie;
+
+    // Use this for initialization
+    void Start () {
+        renderer.material.mainTexture = movie;
+        movie.Play();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    }
+}
---
+++ Assets/Scripts/WaitAndLoad.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaitAndLoad : MonoBehaviour {
+    public string scene;
+    public float delay;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(WaitLoad());
+    }
+
+    public IEnumerator WaitLoad() {
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel(scene);
+    }
+    
+}
