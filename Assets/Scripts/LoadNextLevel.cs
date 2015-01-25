fc4af69ea244f4d296eff5f763d95bbf6b4c2774
Hugo Puhlmann <hugopuhlmann@gmail.com>
2015-01-25 13:20:46 -0200

    Adding thing to allow us loading the next level.

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNextLevel : MonoBehaviour {
+    public float delay;
+    public string nextScene;
+
+    public void OnTriggerEnter2D(Collider2D coll) {
+        if (coll.GetComponent<PlayerController>() != null) StartCoroutine(WaitAndLoad());
+    }
+
+    public IEnumerator WaitAndLoad() {
+        yield return new WaitForSeconds(delay);
+        Application.LoadLevel(nextScene);
+    }
+}
